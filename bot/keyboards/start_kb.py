2c483745571f6a68cae6b99ce7638efef80c1a53
caz1que <okayokayokayigotcha@gmail.com>
2025-06-18 23:39:12 +0300

    fix: fix start keyboard buttons

--- bot/keyboards/start_kb.py
+++ bot/keyboards/start_kb.py
@@ -1,15 +1,11 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import emoji
 
-option_1 = emoji.emojize(":framed_picture:") + " Создать изображение"
-option_2 = emoji.emojize(":newspaper:") + " Новости"
-option_3 = emoji.emojize(":house_with_garden:") + " Развлекательный контент"
+option_1 = emoji.emojize(":newspaper:") + " Смотреть новости"
 
 def start_kb():
     buttons = [
-        [InlineKeyboardButton(text=option_1, callback_data="create_image")],
-        [InlineKeyboardButton(text=option_2, callback_data="news")],
-        [InlineKeyboardButton(text=option_3, callback_data="fun")],
+        [InlineKeyboardButton(text=option_1, callback_data="news")]
     ]
     keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
     return keyboard
